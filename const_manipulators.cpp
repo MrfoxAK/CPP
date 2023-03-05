42852390b3b0e419206b5a6c4c97b7f6bff6c3e4
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2023-03-05 22:13:49 +0530

    Add files via upload

---
+++ const_manipulators.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+     // int a = 4;
+     // cout<<"a is "<<a<<endl;
+     // a = 5;
+     // cout<<"a is "<<a<<endl;
+     const int a = 4;
+     cout<<"a is "<<a<<endl;
+     // a = 5;
+     // cout<<"a is "<<a<<endl;
+     int ac = 132, b = 56;
+     cout<<"using setw"<<setw(16)<<ac;
+}
